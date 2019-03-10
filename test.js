7b7b35548d34c8e4d45e64d28ec4313e492346b4
evanhackett <evanwhackett@gmail.com>
2019-03-09 20:23:35 -0800

    created npm module and added tape for testing.

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
+const test = require('tape')
 const meta2 = require('./meta2')
 const src_input = require('./input1')
 const interpreter_input = require('./inputcode1')
 
 // we are expecting the output to be equal to the interpreter_input,
 // meaning we expect the meta2 interpreter to generate itself.
-console.log(meta2(src_input, interpreter_input) == interpreter_input)
+test('meta2 should generate itself', t => {
+  t.equal(meta2(src_input, interpreter_input), interpreter_input)
+  t.end()
+})
